f63b59103ef920a8cd5ab0caebffb894e1dd1866
niverred <anisa.zikra@gmail.com>
2023-05-17 00:03:30 +0700

    update

--- cycloid.py
+++ cycloid.py
@@ -6,6 +6,7 @@ pygame.font.init()
 
 # Define the background colour using RGB color coding
 background_colour = (255, 255, 255)
+header_colour = (192, 192, 192)  # Gray color for the header
 
 # Define the dimensions of the screen object (width, height)
 screen_width = 1440
@@ -13,7 +14,7 @@ screen_height = 950
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Set the caption of the screen
-pygame.display.set_caption('Cycloid Curve')
+pygame.display.set_caption('Optimochrone')
 
 # Fill the background colour to the screen
 screen.fill(background_colour)
@@ -21,8 +22,26 @@ screen.fill(background_colour)
 # Set up the font
 font = pygame.font.Font(None, 28)
 
+# Render the header text
+header_text = font.render('Optimochrone', True, pygame.Color('black'))
+
+# Calculate the position to align the header text at the top
+header_x = 0
+header_y = 0
+
+# Calculate the dimensions of the header based on the text size
+header_width = screen_width
+header_height = 40
+
+# Draw the header rectangle with the gray color
+header_rect = pygame.Rect(header_x, header_y, header_width, header_height)
+pygame.draw.rect(screen, header_colour, header_rect)
+
+# Blit the header text onto the screen
+screen.blit(header_text, (header_x + 5, header_y + 5))
+
 # Load the image of the cycloid curve
-curve_image = pygame.image.load('cycloid_curve.png')
+curve_image = pygame.image.load('Screenshot 2023-05-16 235054.png')
 
 # Get the dimensions of the curve image
 curve_image_width = curve_image.get_width()
@@ -30,14 +49,14 @@ curve_image_height = curve_image.get_height()
 
 # Calculate the position to align the image on the right side
 image_x = screen_width - curve_image_width - 25
-image_y = 300
+image_y = header_y + header_height + 25
 
 # Blit the curve image onto the screen
 screen.blit(curve_image, (image_x, image_y))
 
 # Cycloid curve information
 curve_info = [
-    "Cycloid Curve",
+     "Cycloid Curve",
     "-----------------",
     " The cycloid curve is a type of trochoid curve.",
     " It is traced by a point on the circumference of a rolling circle.",
@@ -53,7 +72,12 @@ curve_info = [
     " The curve has symmetry with respect to both the x-axis and the y-axis.",
     " The length of one arch of the cycloid curve is 8r.",
     " Applications of the cycloid curve include gear design and pendulum motion.",
+
 ]
+
+# Calculate the position to align the text on the left side
+text_x = header_x
+text_y = image_y
 
 # Render the text on separate surfaces
 text_surfaces = []
@@ -61,14 +85,10 @@ for line in curve_info:
     text_surface = font.render(line, True, pygame.Color('black'))
     text_surfaces.append(text_surface)
 
-# Calculate the position to align the text on the left side
-text_x = 25
-text_y = 25
-
 # Blit the text surfaces onto the screen
 for text_surface in text_surfaces:
     screen.blit(text_surface, (text_x, text_y))
-    text_y += 30
+    text_y += text_surface.get_height() + 10
 
 # Update the display using flip
 pygame.display.flip()
